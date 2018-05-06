68efdd4ff8c2dbf36dfcb94eea586df5dd6b5b5f
Gibran Otazo <gibran@entropy.tech>
2018-05-06 17:12:28 -0500

    Update graph.scala

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -1,16 +1,12 @@
 package dhclust
 
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.linalg.distributed.RowMatrix
-
 object Graph {
 
-    def adjacencyMatrix(v: org.apache.spark.mllib.linalg.Vector): Array[org.apache.spark.mllib.linalg.Vector] = {
+    def adjacencyMatrix(v: Array[Int]): Array[Array[Int]] = {
        val n = v.size - 1
-       var A = Array(Vectors.zeros(n+1))
+       var A = Array(Array.fill(n+1)(0))
        for( k <- 1 to n){
-          A =  A ++ Array(Vectors.zeros(n+1))
+          A =  A ++ Array(Array.fill(n+1)(0))
        }
 
        var index = Array[Int]()
@@ -23,44 +19,44 @@ object Graph {
        for(i <- index){
           for(j <- index){
              if (i < j){
-                A(i).toArray(j) = 1
-                A(j).toArray(i) = 1
+                A(i)(j) = 1
+                A(j)(i) = 1
              }
           }
        }
        return A
     }
 
-    def sumAllEntries(A: Array[org.apache.spark.mllib.linalg.Vector]): Double = {
+    def sumAllEntries(A: Array[Array[Int]]): Double = {
        var sum1 = 0.00
        for( i <- A){
-          for(j <- i.toArray){
+          for(j <- i){
               sum1 = sum1 + j
           }
        }
        return sum1
     }
 
-    def degrees(A: Array[org.apache.spark.mllib.linalg.Vector]): org.apache.spark.mllib.linalg.Vector = {
+    def degrees(A: Array[Array[Int]]): Array[Int] = {
         var n = A.size
-        var out = Vectors.zeros(n)
+        var out = Array.fill(n)(0)
         for( i <- A){
            for(j <- 0 to (i.size-1)){
-               out.toArray(j) = out.toArray(j) + i.toArray(j)
+               out(j) = out(j) + i(j)
            }
         }
        return out
     }
 
-    def aggregate(A: Array[org.apache.spark.mllib.linalg.Vector], B: Array[org.apache.spark.mllib.linalg.Vector]): Array[org.apache.spark.mllib.linalg.Vector] = {
+    def aggregate(A: Array[Array[Int]], B: Array[Array[Int]]): Array[Array[Int]] = {
         var n = A.size
-        var out = Array[org.apache.spark.mllib.linalg.Vector]()
+        var out = Array[Array[Int]]()
         for( i <- 0 to (n-1)){
-           var x = Vectors.zeros(n)
+           var x = Array.fill(n)(0)
            for(j <- 0 to (n-1)){
-               x.toArray(j) = A(i).toArray(j) + B(i).toArray(j)
-               if(x.toArray(j) > 1){
-                  x.toArray(j) = 1
+               x(j) = A(i)(j) + B(i)(j)
+               if(x(j) > 1){
+                  x(j) = 1
                }
            }
            out = out ++ Array(x)
