fb39f3c28004ccba6009cd9558a61589824fd492
Gibran Otazo <gibran@entropy.tech>
2018-05-13 19:52:47 -0500

    Update tdm.scala

--- src/main/scala/tdm.scala
+++ src/main/scala/tdm.scala
@@ -5,15 +5,6 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object TM {
-  
-  def zeros(n: Int): scala.collection.mutable.Map[Int, Double] = {
-    var x = scala.collection.mutable.Map[Int, Double]()
-    for(i <- 0 to n-1){
-      x = x + (i -> 0.00)
-    }
-    return x
-  }
-  
   def termDocumentMatrix(texts: org.apache.spark.rdd.RDD[String], sc: SparkContext): scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]] = {
     var tokens = texts.map(x => x.split(" "))
     var dwords = sc.broadcast(tokens.flatMap(token => token).distinct().collect)
@@ -22,12 +13,10 @@ object TM {
     var documents = sc.broadcast(tokens.collect)
     var k = 0
     for( i <- documents.value){
-       var x = zeros(n)
        for( j <- i){
          var index = dwords.value.indexOf(j)
-         x(index) = 1.00
+         tdm(k)(index) = 1.00
        }
-       tdm(k) = x
        k = k + 1
     }
     return tdm
