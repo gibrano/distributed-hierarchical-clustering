8bab7b8223694f9fd5d3a99594173faec5180e8e
Gibran Otazo <gibran@entropy.tech>
2018-05-20 18:14:49 -0500

    Update jsdivergence.scala

--- src/main/scala/jsdivergence.scala
+++ src/main/scala/jsdivergence.scala
@@ -6,25 +6,18 @@ import org.apache.spark.SparkConf
 
 object Divergence {
 
-  def JensenShannon(A: org.apache.spark.rdd.RDD[Array[Double]], B: org.apache.spark.rdd.RDD[Array[Double]], sc: SparkContext): Double = {
-     var A2 = A.collect
-     var B2 = B.collect
-     var n = A2.size - 1
-     var C = Array[Array[Double]]()
-     for(i <- 0 to n){
-        var x = Array.fill(n+1)(0.00)
-        for(j <- 0 to n){
-           x(j) = 0.5*(A2(i)(j) - B2(i)(j))
-        }
-        C = C ++ Array(x)
+  def JensenShannon(A: Array[Double], B: Array[Double]): Double = {
+     var n = A.size
+     var C = Array.fill(n)(0.00)
+     for(i <- 0 to (n-1)){
+       C(i) = 0.5*(A(i)+B(i))
      }
-     var C2 = sc.parallelize(C)
-     var r = Entropy.VonNewmann(C2, sc)-(1/2)*(Entropy.VonNewmann(A, sc)+Entropy.VonNewmann(B, sc))
+     var r = Entropy.VonNewmann(C)-(1/2)*(Entropy.VonNewmann(A)+Entropy.VonNewmann(B))
      return r
   }
 
-  def computeJSD(x: Array[Int], layers: Array[org.apache.spark.rdd.RDD[Array[Double]]], sc: SparkContext) : Double = {
-    var jsd = JensenShannon(layers(x(0)),layers(x(1)), sc)
+  def computeJSD(x: Array[Int], tdm: Array[Array[Double]]) : Double = {
+    var jsd = JensenShannon(tdm(x(0)),tdm(x(1)))
     return jsd
   }
 
