e549438f09a01e4bc19a346d2c6cc1a503e8168d
Gibran Otazo <gibran@entropy.tech>
2018-05-20 13:13:27 -0500

    Update graph.scala

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -6,30 +6,29 @@ import org.apache.spark.SparkConf
 
 object Graph {
 
-    def adjacencyMatrix(v: scala.collection.mutable.Map[Int, Double]): scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]] = {
+    def adjacencyMatrix(v: Array[Double]): Array[Double] = {
        val n = v.size
-       var A = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]()
+       var A = Array.fill(n*n)(0.00)
        for(i <- 0 to (n-1)){
          for(j <- i to (n-1)){  
            if(i != j && v(i) >= 1.00 && v(j) >= 1.00){
-             A.update(i,scala.collection.mutable.Map[Int,Double](j -> 1.00))
-             A.update(j,scala.collection.mutable.Map[Int,Double](i -> 1.00))
-           }  
+             A(i*n+j) = 1.00
+             A(j*n+i) = 1.00  
+           } 
          }
        }
        return A
     }
 
-    def aggregate(A:  scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]], B:  scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]):  scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]] = {
+    def aggregate(A:  Array[Double], B:  Array[Double]): Array[Double] = {
       var n = A.size
+      var C = Array.fill(n)(0.00)
       for( i <- 0 to (n-1)){
-        for(j <- i to (n-1)){
-          var aij = A.getOrElse(i,scala.collection.mutable.Map[Int,Double](j -> 0.00)).getOrElse(j, 0.00)
-          var bij = B.getOrElse(i,scala.collection.mutable.Map[Int,Double](j -> 0.00)).getOrElse(j, 0.00)
-          A.update(i, scala.collection.mutable.Map[Int,Double](j -> (aij+bij)))
-          A.update(j, scala.collection.mutable.Map[Int,Double](i -> (aij+bij)))
-        }
+        var cij = A(i) + B(i)
+        if(cij > 0){  
+          C(i) = cij/cij
+        }    
       }
-      return A
+      return C
     }
 }
