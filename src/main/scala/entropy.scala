db6e80c5092deccd41bdf8b533dfe21df113f9f0
Gibran Otazo <gibran@entropy.tech>
2018-05-20 17:33:54 -0500

    Update entropy.scala

--- src/main/scala/entropy.scala
+++ src/main/scala/entropy.scala
@@ -6,33 +6,31 @@ import org.apache.spark.SparkConf
 
 object Entropy {
 
-    def VonNewmann(A: scala.collection.mutable.Map[Int, Double], sc: SparkContext): Double = {
+    def VonNewmann(doc: Array[Double]): Double = {
+        var n = doc.size
         var entropy = 0.00
+        var numberwords = doc.sum
+        var degree = numberwords - 1
         var out = 0.00
-        val E = A.values.sum // sum of edges
+        var E = degree*(degree+1)/2.00 // # of edges (sum of n = n*(n+1)/2) 
         if (E != 0){
-            val c = 1.00/(2.00*E)
-            var index=sc.parallelize(0 to (A.size-1))
-            val n = A.size - 1
-            for(i <- 0 to ){
-              var d = index.map(j => A(j)(i)).reduce((x,y) => x+y)
-              var degr = degr ++ Array(d)   
-            }
-            val degr = A.map(r => r.sum).collect // degrees of nodes
-            var A2 = A.collect
-            
+            var c = 1.00/(2*E)
             var L = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]()
-            for(i <- 0 to n){
-                var x = scala.collection.mutable.Map[Int,Double]()
-                for(j <- i to n){
+            for(i <- 0 to (n-1)){
+              L(i) =  scala.collection.mutable.Map[Int,Double]()
+            }    
+            
+            for(i <- 0 to (n-1)){
+                for(j <- i to (n-1)){
                    if(i == j){
-                     x(j) = c*(degr(i) - A2(i)(j))
-                   } else {
-                     x(j) = -c*A2(i)(j)
+                     L(i)(j) = c*degree*doc(i)
+                   } else if(doc(i) >= 1.00 && doc(j) >= 1.00){
+                       L(i)(j) = -c
+                       L(j)(i) = -c
                    } 
                 }
-                L = L ++ Array(x)
             }
+            
             val eigen = Decomposition.eigenValues(L)
             for(s <- eigen){
                 entropy += -s*math.log(s)
@@ -41,14 +39,14 @@ object Entropy {
         return entropy
     }
 
-    def relative(layers: Array[org.apache.spark.rdd.RDD[Array[Double]]], sc: SparkContext): Double = {
-       var n = layers.size - 1
-       var H = layers.map(C => VonNewmann(C, sc)).reduce((x,y) => x + y)
+    def relative(tdm: Array[Array[Double]]): Double = {
+       var n = tdm.size - 1
+       var H = tdm.map(doc => VonNewmann(doc)).reduce((x,y) => x + y)
        return H/(n+1)
     }
 
-    def GlobalQuality(layers: Array[org.apache.spark.rdd.RDD[Array[Double]]], hA: Double, sc: SparkContext): Double = {
-       var q = 1.00 - relative(layers, sc)/hA
+    def GlobalQuality(layers: Array[Array[Double]], hA: Double): Double = {
+       var q = 1.00 - relative(layers)/hA
        return q
     }
 
