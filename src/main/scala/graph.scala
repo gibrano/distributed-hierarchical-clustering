0890ae7decc0727054d88b8dd06260e0d10ea817
Gibran Otazo <gibran@entropy.tech>
2018-06-06 21:30:02 -0500

    Update graph.scala

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -4,31 +4,38 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-object Graph {
+object Graph extends Serializable {
 
-    def adjacencyMatrix(v: Array[Double]): Array[Double] = {
+    def adjacencyMatrix(v: Array[Int]): Array[Array[Double]] = {
        val n = v.size
-       var A = Array.fill(n*n)(0.00)
-       for(i <- 0 to (n-1)){
-         for(j <- i to (n-1)){  
-           if(i != j && v(i) >= 1.00 && v(j) >= 1.00){
-             A(i*n+j) = 1.00
-             A(j*n+i) = 1.00  
-           } 
-         }
+       var A = Array(Array.fill(n)(0.00))
+       for( k <- 1 to (n-1)){
+          A =  A ++ Array(Array.fill(n)(0.00))
+       }
+       for(i <- 0 to (n-2)){
+          for(j <- (i+1) to (n-1)){
+             if (v(i) >= 1.00 && v(j) >= 1.00){
+                A(i)(j) = 1
+                A(j)(i) = 1
+             }
+          }
        }
        return A
     }
 
-    def aggregate(A:  Array[Double], B:  Array[Double]): Array[Double] = {
-      var n = A.size
-      var C = Array.fill(n)(0.00)
-      for( i <- 0 to (n-1)){
-        var cij = A(i) + B(i)
-        if(cij > 0){  
-          C(i) = cij/cij
-        }    
-      }
-      return C
+    def aggregate(A: Array[Array[Double]], B: Array[Array[Double]]): Array[Array[Double]] = {
+        var out = Array[Array[Double]]()
+        var n = A.size
+        for( i <- 0 to (n-1)){
+           var x = Array.fill(n)(0.00)
+           for(j <- 0 to (n-1)){
+               x(j) = A(i)(j) + B(i)(j)
+               if(x(j) > 1){
+                  x(j) = 1
+               }
+           }
+           out = out ++ Array(x)
+        }
+        return out
     }
 }
