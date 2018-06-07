c5b78c912bea86f36aadcfbd811de17f7a713b4e
Gibran Otazo <gibran@entropy.tech>
2018-06-06 23:20:01 -0500

    Update jsdivergence.scala

--- src/main/scala/jsdivergence.scala
+++ src/main/scala/jsdivergence.scala
@@ -6,18 +6,25 @@ import org.apache.spark.SparkConf
 
 object Divergence extends Serializable {
 
-  def JensenShannon(A: Array[Double], B: Array[Double]): Double = {
+  def JensenShannon(A: Array[Array[Double]], B: Array[Array[Double]]): Double = {
+     var C = Array[Array[Double]]()
      var n = A.size
-     var C = Array.fill(n)(0.00)
      for(i <- 0 to (n-1)){
-       C(i) = 0.5*(A(i)+B(i))
+       var x = Array.fill(n)(0.00)
+       for(j <- 0 to (n-1)){
+         x(j) = 0.5*(A(i)(j)+B(i)(j))
+         if(x(j) > 1){
+           x(j) = 1
+         }
+       }
+       C = C ++ Array(x)
      }
      var r = Entropy.VonNewmann(C)-(1/2)*(Entropy.VonNewmann(A)+Entropy.VonNewmann(B))
      return r
   }
 
-  def computeJSD(x: Array[Int], tdm: Array[Array[Double]]) : Double = {
-    var jsd = JensenShannon(tdm(x(0)),tdm(x(1)))
+  def computeJSD(x: Array[Int], layers: Array[Array[Array[Double]]]) : Double = {
+    var jsd = JensenShannon(layers(x(0)),layers(x(1)))
     return jsd
   }
 
