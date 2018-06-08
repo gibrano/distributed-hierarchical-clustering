d3df520ae0c1b8b54d60df18a6e5f4cad99dc9ce
Gibran Otazo <gibran@entropy.tech>
2018-06-07 23:06:42 -0500

    Update entropy.scala

--- src/main/scala/entropy.scala
+++ src/main/scala/entropy.scala
@@ -6,47 +6,84 @@ import org.apache.spark.SparkConf
 
 object Entropy extends Serializable {
 
-    def VonNewmann(layer: Array[Array[Double]]): Double = {
-        var A = layer
-        var n = layer.size
-        var entropy = 0.00
-        var sumall = layer.map(row => row.sum).reduce((x,y) => x+y)
-        var dgr = layer.map(row => row.sum)
-        if (sumall != 0){
-            var c = 1.00/sumall
-            var L = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]()
-            for(i <- 0 to (n-1)){
-              L(i) =  scala.collection.mutable.Map[Int,Double]()
-            }    
-            
-            for(i <- 0 to (n-1)){
-                for(j <- i to (n-1)){
-                   if(i == j){
-                     L(i)(j) = c*(dgr(i) - A(i)(j))
-                   } else {
-                     L(i)(j) = -c*A(i)(j)
-                     L(j)(i) = -c*A(j)(i)
-                   } 
-                }
-            }
-            
-            val eigen = Decomposition.eigenValues(L)
-            for(s <- eigen){
-                entropy += -s*math.log(s)
-            }
+    def VonNewmann1(layer: Array[Array[Double]]): Double = {
+      var A = layer
+      var n = layer.size
+      var entropy = 0.00
+      var sumall = layer.map(row => row.sum).reduce((x,y) => x+y)
+      var dgr = layer.map(row => row.sum)
+      if (sumall != 0){
+        var c = 1.00/sumall
+        var L = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]()
+        for(i <- 0 to (n-1)){
+          L(i) =  scala.collection.mutable.Map[Int,Double]()
+        }    
+         
+        for(i <- 0 to (n-1)){
+          for(j <- i to (n-1)){
+            if(i == j){
+              L(i)(j) = c*(dgr(i) - A(i)(j))
+            } else {
+              L(i)(j) = -c*A(i)(j)
+              L(j)(i) = -c*A(j)(i)
+            } 
+          }
         }
-        return entropy
+            
+        val eigen = Decomposition.eigenValues(L)
+        for(s <- eigen){
+          entropy += -s*math.log(s)
+        }
+      }
+      return entropy
     }
-
-    def relative(layers: Array[Array[Array[Double]]]): Double = {
-       var X = layers.size
-       var H = layers.map(layer => VonNewmann(layer)).reduce((x,y) => x + y)
-       return H/X
+    
+    def VonNewmann2(layer: Array[Array[Double]], par: Array[Double]): Double = {
+      var A = layer
+      var n = layer.size
+      var entropy = 0.00
+      var sumall = layer.map(row => row.sum).reduce((x,y) => x+y)
+      var dgr = layer.map(row => row.sum)
+      if (sumall != 0){
+        var c = 1.00/sumall
+        var L = Array[Array[Double]]()  
+        for(i <- 0 to (n-1)){
+          var x = Array.fill(n)(0.00)
+          for(j <- 0 to (n-1)){
+            if(i == j){
+              x(j) = c*(dgr(i) - A(i)(j))
+            } else {
+              x(j) = -c*A(i)(j)
+            } 
+          }
+          L = L ++ Array(x)
+        }
+        entropy = - par(0)*L.size - par(1)*TracePowMatrix(L,1) - par(2)*TracePowMatrix(L,2)
+      }
+      return entropy
     }
-
-    def GlobalQuality(layers: Array[Array[Array[Double]]], hA: Double): Double = {
-       var q = 1.00 - relative(layers)/hA
-       return q
+    
+    def TracePowMatrix(A: Array[Array[Double]], pow: Int): Double = {
+      var n = A.size
+      var trace = 0.0
+      if(pow == 1){  
+        for(i <- 0 to (n-1)){
+          trace = trace + vectorProd(A(i),A(i))
+        }
+      } else if(pow == 2){
+        for(i <- 0 to (n-1)){
+          trace = trace + A(i)(i)
+        }
+      }    
+      return trace  
     }
-
+         
+    def vectorProd(x: Array[Double], y: Array[Double]): Double = {
+       var n = x.size
+       var sum = 0.00 
+       for(i <- 0 to (n-1)){
+          sum = sum + x(i)*y(i)
+       }
+       return sum 
+    }     
 }
