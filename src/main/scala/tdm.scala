df11c42fff100a8ce3ec2c713e5587dda0b84749
Gibran Otazo <gibran@entropy.tech>
2018-06-10 21:39:17 -0500

    Update tdm.scala

--- src/main/scala/tdm.scala
+++ src/main/scala/tdm.scala
@@ -6,7 +6,8 @@ import org.apache.spark.SparkConf
 
 object TM extends Serializable {
   def termDocumentMatrix(texts: org.apache.spark.rdd.RDD[String], sc: SparkContext): Array[Array[Double]] = {
-    var tokens = texts.map(x => x.split(" "))
+    var corpus = sc.parallelize(texts)
+    var tokens = corpus.map(x => x.split(" ")).cache()
     var dwords = sc.broadcast(tokens.flatMap(token => token).distinct().collect)
     val n = dwords.value.size
     var tdm = Array[Array[Double]]()
