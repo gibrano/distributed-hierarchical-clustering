6c6ff814d5b5baa4193a4c2b0e3567d1afa18dd9
Gibran Otazo <gibran@entropy.tech>
2018-06-11 21:27:54 -0500

    Update tdm.scala

--- src/main/scala/tdm.scala
+++ src/main/scala/tdm.scala
@@ -5,19 +5,21 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 object TM extends Serializable {
-  def termDocumentMatrix(texts: org.apache.spark.rdd.RDD[String], sc: SparkContext): Array[Array[Double]] = {
+  def termDocumentMatrix(texts: org.apache.spark.rdd.RDD[String], sc: SparkContext): scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]] = {
     var tokens = texts.map(x => x.split(" ")).cache()
     var dwords = sc.broadcast(tokens.flatMap(token => token).distinct().collect)
     val n = dwords.value.size
-    var tdm = Array[Array[Double]]()
+    var tdm = scala.collection.mutable.Map[Int, scala.collection.mutable.Map[Int,Double]]()
     var documents = sc.broadcast(tokens.collect)
-    for(i <- documents.value){
-      var x = Array.fill(n)(0.00)
-       for(j <- i){
+    var i = 0
+    for(doc <- documents.value){
+      var x = scala.collection.mutable.Map[Int,Double]()
+       for(j <- doc){
          var index = dwords.value.indexOf(j)
          x(index) = 1.00
        }
-       tdm = tdm ++ Array(x)
+       tdm(i) = x
+      i = i + 1
     }
     return tdm
   }
