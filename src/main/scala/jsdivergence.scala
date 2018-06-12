a2702b59a2350980430b7e4df474d41880a0f865
Gibran Otazo <gibran@entropy.tech>
2018-06-11 23:46:33 -0500

    Update jsdivergence.scala

--- src/main/scala/jsdivergence.scala
+++ src/main/scala/jsdivergence.scala
@@ -6,22 +6,22 @@ import org.apache.spark.SparkConf
 
 object Divergence extends Serializable {
   
-  def JensenShannon(A: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]], B: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]],par: Array[Double]): Double = {
+  def JensenShannon(A: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]], B: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]],par: Array[Double], n: Int): Double = {
     var C = Graph.aggregate(A,B)
     for(i <- C.keys){
       for(j <- C(i).keys){
         C(i)(j) = 0.5*C(i)(j)
       }  
     }
-    var entropyA = Entropy.VonNewmann2(A,par)
-    var entropyB = Entropy.VonNewmann2(B,par)
-    var entropyC = Entropy.VonNewmann2(C,par)
+    var entropyA = Entropy.VonNewmann2(A,par,n)
+    var entropyB = Entropy.VonNewmann2(B,par,n)
+    var entropyC = Entropy.VonNewmann2(C,par,n)
     var r = entropyC-(1/2)*(entropyA+entropyB)
     return r
   }  
   
-  def computeJSD(x: Array[Int], layers: Array[scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]], par: Array[Double]) : Double = {
-    var jsd = JensenShannon(layers(x(0)),layers(x(1)),par)
+  def computeJSD(x: Array[Int], layers: Array[scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]], par: Array[Double], n: Int) : Double = {
+    var jsd = JensenShannon(layers(x(0)),layers(x(1)),par,n)
     return jsd
   }
 
