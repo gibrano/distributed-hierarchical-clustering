bab2a01f05cb65ecd3f110581d9ebbd481390cda
Gibran Otazo <gibran@entropy.tech>
2018-06-11 23:02:59 -0500

    Update graph.scala

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -6,36 +6,33 @@ import org.apache.spark.SparkConf
 
 object Graph extends Serializable {
 
-    def adjacencyMatrix(v: Array[Double]): Array[Array[Double]] = {
-       val n = v.size
-       var A = Array(Array.fill(n)(0.00))
-       for( k <- 1 to (n-1)){
-          A =  A ++ Array(Array.fill(n)(0.00))
-       }
-       for(i <- 0 to (n-2)){
-          for(j <- (i+1) to (n-1)){
-             if (v(i) >= 1.00 && v(j) >= 1.00){
-                A(i)(j) = 1
-                A(j)(i) = 1
-             }
-          }
-       }
+    def adjacencyMatrix(v: scala.collection.mutable.Map[Int,Double]): scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]] = {
+       var A = scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]()
+       for(i <- v){
+         A(i._1) = scala.collection.mutable.Map[Int,Double]() 
+         for(j <- v){
+           if(i._1 != j._1){  
+             A(i._1)(j._1) = 1.00
+           }    
+         }
+       } 
        return A
     }
 
-    def aggregate(A: Array[Array[Double]], B: Array[Array[Double]]): Array[Array[Double]] = {
-        var out = Array[Array[Double]]()
-        var n = A.size
-        for( i <- 0 to (n-1)){
-           var x = Array.fill(n)(0.00)
-           for(j <- 0 to (n-1)){
-               x(j) = A(i)(j) + B(i)(j)
-               if(x(j) > 1){
-                  x(j) = 1
-               }
-           }
-           out = out ++ Array(x)
+    def aggregate(A: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]], B: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]): scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]] = {
+        var C = scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]()
+        var keys = A.keys ++ B.keys
+        var e = keys.toArray.distinct
+        for(i <- e){
+          C(i) = scala.collection.mutable.Map[Int,Double]()
+          for(j <- e){
+            var cij = A.getOrElse(i,scala.collection.mutable.Map[Int,Double]()).getOrElse(j,0.00) + B.getOrElse(i,scala.collection.mutable.Map[Int,Double]()).getOrElse(j,0.00)
+            if(cij >= 1 && i != j){
+              C(i)(j) = 1.00
+            }
+          }
         }
-        return out
+        return C
     }
+
 }
