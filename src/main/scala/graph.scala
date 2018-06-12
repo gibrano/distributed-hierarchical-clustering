aeb82cc6c610ab54b2dc70c5d61ce84aa7f40922
Gibran Otazo <gibran@entropy.tech>
2018-06-12 16:14:55 -0500

    Update graph.scala

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -6,33 +6,24 @@ import org.apache.spark.SparkConf
 
 object Graph extends Serializable {
 
-    def adjacencyMatrix(v: scala.collection.mutable.Map[Int,Double]): scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]] = {
-       var A = scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]()
+    def adjacencyMatrix(v: scala.collection.mutable.Map[Int,Double]): Array[Array[Double]] = {
+       var A = Array[Array[Double]]()
        for(i <- v){
-         A(i._1) = scala.collection.mutable.Map[Int,Double]() 
          for(j <- v){
-           if(i._1 != j._1){  
-             A(i._1)(j._1) = 1.00
+           if(i._1 < j._1){
+             A = A ++ Array(Array(i._1,j._1))  
            }    
          }
        } 
        return A
     }
 
-    def aggregate(A: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]], B: scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]): scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]] = {
-        var C = scala.collection.mutable.Map[Int,scala.collection.mutable.Map[Int,Double]]()
-        var keys = A.keys ++ B.keys
-        var e = keys.toArray.distinct
-        for(i <- e){
-          C(i) = scala.collection.mutable.Map[Int,Double]()
-          for(j <- e){
-            var cij = A.getOrElse(i,scala.collection.mutable.Map[Int,Double]()).getOrElse(j,0.00) + B.getOrElse(i,scala.collection.mutable.Map[Int,Double]()).getOrElse(j,0.00)
-            if(cij >= 1 && i != j){
-              C(i)(j) = 1.00
-            }
-          }
-        }
-        return C
+    def aggregate(A: Array[Array[Double]], B: Array[Array[Double]]): Array[Array[Double]] = {
+      var C = Array[Array[Double]]()
+      var keys = A.keys ++ B.keys
+      var C = A ++ B
+      C = C.distinct()
+      return C
     }
 
 }
