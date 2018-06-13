089df48eeee4edf6d02e62c1b6870ffaba222518
Gibran Otazo <gibran@entropy.tech>
2018-06-13 15:30:57 -0500

    Update dhclust.scala

--- src/main/scala/dhclust.scala
+++ src/main/scala/dhclust.scala
@@ -23,7 +23,7 @@ object Clusters extends Serializable {
     for(i <- 1 to (l-1)){
        A = Graph.aggregate(A,C(i))
     }
-    
+    println("A = "+ A)
     println("Computing taylor coefficients  ...")
     var K = A.size
     var sumall = 2*K
@@ -83,6 +83,7 @@ object Clusters extends Serializable {
       q = q ++ Array(globalquality)
       linkages = linkages ++ Array(Array(a,b,globalquality))
     }
+    println("C = "+ C)
     return linkages
   }
 }
