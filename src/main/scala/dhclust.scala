3e8d0f1f5339ad4f8f9120f37537119bfdb4da32
Gibran Otazo <gibran@entropy.tech>
2018-07-28 21:26:44 -0500

    Update dhclust.scala

--- src/main/scala/dhclust.scala
+++ src/main/scala/dhclust.scala
@@ -27,19 +27,20 @@ object Clusters extends Serializable {
   }
   
   def getPairs(layers: Array[Array[Array[Double]]], par: Array[Double], n: Int, sc: SparkContext): Array[Array[Int]] = {
+      var C = layers
       var pairs = Array[Array[Int]](Array[Int]())
       var c = 9
-      while(layers.size > 0){
-        var l = layers.size
+      while(C.size > 0){
+        var l = C.size
         if(l < 20){
           c = l
         }
         var index = (1 to (l-1))
-        var jsdMatrix = sc.parallelize(index).map(i => Divergence.computeJSD(Array(0,i),layers,par,n)).cache()
+        var jsdMatrix = sc.parallelize(index).map(i => Divergence.computeJSD(Array(0,i),C,par,n)).cache()
         var x = jsdMatrix.zipWithIndex().sortByKey(true, 1).take(c)
         var y = x.map(i => i._2.toInt) ++ Array(0)
         pairs = pairs ++ Combine(y)
-        layers = sc.parallelize(y).map(i => layers.apply(i)).collect 
+        C = sc.parallelize(y).map(i => C.apply(i)).collect 
       }
       pairs = pairs.filter(_.size > 0)
       return pairs
