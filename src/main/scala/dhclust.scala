5884fb5961db47ad3bba47836e1b2ac041c1a9f7
Gibran Otazo <gibran@entropy.tech>
2018-08-02 13:55:16 -0500

    Update dhclust.scala

--- src/main/scala/dhclust.scala
+++ src/main/scala/dhclust.scala
@@ -29,10 +29,10 @@ object Clusters extends Serializable {
   def getPairs(layers: Array[Array[Array[Double]]], par: Array[Double], n: Int, sc: SparkContext): Array[Array[Int]] = {
       var C = layers
       var pairs = Array[Array[Int]](Array[Int]())
-      var c = 100
+      var c = math.floor(0.25*C.size).toInt
       while(C.size > 0){
         var l = C.size
-        if(l < 120){
+        if(l < 1.25*c){
           c = l
         }
         var t = System.nanoTime
