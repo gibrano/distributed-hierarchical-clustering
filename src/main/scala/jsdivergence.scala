645f380642f51547329b199e1e41473b8c88d5d4
Gibran Otazo <gibran@entropy.tech>
2018-08-03 14:10:27 -0500

    Update jsdivergence.scala

--- src/main/scala/jsdivergence.scala
+++ src/main/scala/jsdivergence.scala
@@ -15,9 +15,22 @@ object Divergence extends Serializable {
     return r
   }  
   
-  def computeJSD(x: Array[Int], layers: Array[Array[Array[Double]]], par: Array[Double], n: Int) : Double = {
-    var jsd = JensenShannon(layers(x(0)),layers(x(1)),par,n)
-    return jsd
+  def computeJSD(x: Array[Int], layers: Array[Array[Array[Double]]], par: Array[Double], n: Int) : (Int, Int, Double) = {
+    var min = JensenShannon(layers(x(0)),layers(x(1)),par,n)
+    var doc1 = 0
+    var doc2 = 1
+    var l = x.size
+    for( i <- 0 to l-2){
+        for(j <- i+1 to l-1){
+          var jsd = JensenShannon(layers(x(i)),layers(x(j)),par,n)
+          if(min > jsd){
+            min = jsd
+            doc1 = i
+            doc2 = j
+          }  
+        }
+    }
+    return (doc1,doc2,min)
   }
 
 }
